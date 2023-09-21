e56dfe7ef4e9586dc58394d08a041572dbbf0d0d
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-09-21 19:36:57 -0300

    feat: create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+const logger = new Logger();
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john_don@default.com',
+      password: 'john123',
+      coin: 'BRL',
+      login: 'john',
+      recoverCode: null,
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    logger.error('Prisma connection error', e);
+    process.exit(1);
+  })
+
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
