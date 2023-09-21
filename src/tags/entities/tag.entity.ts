e35630547ac66d5d2da67cd001605e336e85b226
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-09-21 10:51:01 -0300

    feat: remove old entities and add entites on prisma

--- src/tags/entities/tag.entity.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractClass } from 'src/database/abstractClass/abstracClass.class';
-import { Revenue } from 'src/revenue/entities/revenue.entity';
-import { User } from 'src/users/entities/user.entity';
-
-export class Tag extends AbstractClass {
-  name: string;
-  user: User;
-  revenues: Revenue[];
-}
