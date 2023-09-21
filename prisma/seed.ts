d5dc965137ffecfbe93a34def6ce4efc09d38de0
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-09-21 19:48:08 -0300

    feat: improve seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,8 +5,12 @@ const logger = new Logger();
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
+  const user = await prisma.user.upsert({
+    where: {
+      email: 'John Doe',
+    },
+    update: {},
+    create: {
       name: 'John Doe',
       email: 'john_don@default.com',
       password: 'john123',
